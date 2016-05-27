80c72cf345b70711dcdcfe67c78e164b7fdb05ff
wangguoxing <wangguoxing@baidu.com>
2016-05-27 11:11:01 +0800

    update

---
+++ common/src/thread/demo/ExecutorServiceTest.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) 2016 Baidu, Inc. All Rights Reserved.
+ */
+package thread.demo;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * $Id ExecutorServiceTest.java 2016-05-27 10:51 wangguoxing@baidu.com $
+ */
+public class ExecutorServiceTest {
+    public static void main(String[] args) throws Exception {
+        // 创建一个固定大小的线程池
+        ExecutorService service = Executors.newFixedThreadPool(3);
+        for (int i = 0; i < 10; i++) {
+            System.out.println("创建线程" + i);
+            Runnable run = new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        Thread.sleep(3000L);
+                        System.out.println("启动线程");
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            };
+            // 在未来某个时间执行给定的命令
+            service.execute(run);
+        }
+        // 关闭启动线程
+        service.shutdown();
+        // 等待子线程结束，再继续执行下面的代码
+        service.awaitTermination(Long.MAX_VALUE, TimeUnit.DAYS);
+        System.out.println("all thread complete");
+    }
+}
---
+++ common/src/thread/demo/ThreadPoolTaskExecutorTest.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright (C) 2016 Baidu, Inc. All Rights Reserved.
+ */
+package thread.demo;
+
+import java.util.ArrayList;
+import java.util.concurrent.Callable;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+/**
+ * $Id ThreadPoolTaskExecutorTest.java 2016-05-27 11:00 wangguoxing@baidu.com $
+ */
+public class ThreadPoolTaskExecutorTest implements Callable<String> {
+
+    private CountDownLatch latch;
+    private String workerName;
+    private long workTime;
+
+    public ThreadPoolTaskExecutorTest(String workerName, long workTime, CountDownLatch latch) {
+        this.workerName = workerName;
+        this.workTime = workTime;
+        this.latch = latch;
+    }
+
+    public String call() {
+        try {
+            Thread.sleep(workTime); //工人开始干活
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            latch.countDown(); //工人完成工作，计数器减一
+        }
+        return "工作者: " + workerName + " 工作了: " + workTime + " s";
+    }
+
+    public static void main(String[] args) throws Exception {
+        ArrayList<Future<String>> results = new ArrayList<Future<String>>();
+        int workerNum = 10; //五个线程的协作10个任务
+        CountDownLatch latch = new CountDownLatch(workerNum);
+        ThreadPoolTaskExecutor taskExecutor = ThreadPoolTaskExecutorTest.getPoolTaskExecutor();
+        for (int k = 0; k < workerNum; k++) {
+            results.add(taskExecutor.submit(new ThreadPoolTaskExecutorTest("name" + k, 3000L, latch)));
+        }
+        if(latch.await(10000, TimeUnit.MILLISECONDS)) {
+            System.out.println("all the threads end!");
+        } else {
+            System.out.println("time out!");
+        }
+        taskExecutor.shutdown();
+        for (Future future : results) {
+            System.out.println(future.get());
+        }
+    }
+
+    public static ThreadPoolTaskExecutor getPoolTaskExecutor() {
+        ThreadPoolTaskExecutor poolTaskExecutor = new ThreadPoolTaskExecutor();
+        //线程池所使用的缓冲队列
+        poolTaskExecutor.setQueueCapacity(200);
+        //线程池维护线程的最少数量
+        poolTaskExecutor.setCorePoolSize(5);
+        //线程池维护线程的最大数量
+        poolTaskExecutor.setMaxPoolSize(1000);
+        //线程池维护线程所允许的空闲时间
+        poolTaskExecutor.setKeepAliveSeconds(30000);
+        poolTaskExecutor.initialize();
+
+        return poolTaskExecutor;
+    }
