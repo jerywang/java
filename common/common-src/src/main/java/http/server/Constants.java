3aea06bd8a6e36f781befd458196bcea2007469e
wangguoxing <wangguoxing@baidu.com>
2016-08-22 16:00:51 +0800

    update http server

--- common/common-src/src/main/java/http/server/Constants.java
+++ common/common-src/src/main/java/http/server/Constants.java
@@ -7,6 +7,12 @@ package http.server;
  * $Id Constants.java Aug 19,2016 wangguoxing@baidu.com $
  */
 public class Constants {
+    // 关闭服务命令
+    public static final String SHUTDOWN_COMMAND = "/shutdown";
+    // 网站静态文件根目录
     public static final String WEB_ROOT = "/home/wangguoxing/baidu/java/common/common-src/src/main/java/http/server";
-    private static final int MAX_THREADS = 50;
+    // http服务最大线程数
+    public static final int MAX_THREADS = 100;
+    // http服务端口
+    public static final int HTTP_PORT = 8080;
 }
